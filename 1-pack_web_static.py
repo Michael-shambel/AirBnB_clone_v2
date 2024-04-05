8f26092f440ff88a55beddb6eabc8398b4bce4c4
michael-shambel <Michaelshambel10@gmail.com>
2024-04-05 06:59:19 -0700

    task 1 erro

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,10 +2,9 @@
 """
  Fabric script that generates a .tgz archive
  from the contents of the web_static folder
- of your AirBnB Clone repo, using the function do_pack.
- """
- from datetime import datetime
- from fabric.api import *
+ of your AirBnB Clone repo, using the function do_pack."""
+from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
