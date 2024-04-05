9702160770ac2c670539718ebdc13c99cbdcbfd4
michael-shambel <Michaelshambel10@gmail.com>
2024-04-05 14:39:57 -0700

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,17 +17,22 @@ def do_deploy(archive_path):
     """
     if not exists(archive_path):
         return False
-    
+
     name = archive_path.split('/')[-1]
     no_ext = name.split('.')[0]
-    
-    put(archive_path, '/tmp/')
-    run('mkdir -p /data/web_static/releases/{}/'.format(no_ext))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(name, no_ext))
-    run('rm /tmp/{}'.format(name))
-    run('mv /data/web_static/releases/{}/web_static/* '
-        '/data/web_static/releases/{}/'.format(no_ext, no_ext))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(no_ext))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ '
-        '/data/web_static/current'.format(no_ext))
+
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}/'.format(no_ext))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            name, no_ext))
+        run('rm /tmp/{}'.format(name))
+        run('mv /data/web_static/releases/{}/web_static/* '
+            '/data/web_static/releases/{}/'.format(no_ext, no_ext))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ '
+            '/data/web_static/current'.format(no_ext))
+        return True
+    except Exception as e:
+        return False
