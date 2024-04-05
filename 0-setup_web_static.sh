8c85e9c21df3df3091af2ac693e5b3656b1773f8
michael-shambel <Michaelshambel10@gmail.com>
2024-04-05 06:23:22 -0700

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,7 +15,13 @@ mkdir -p /data/web_static/shared/
 # Create the folder /data/web_static/releases/test/ if it doesn’t already exist
 mkdir -p /data/web_static/releases/test/
 # Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
-echo "test MY Nginx configuration" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 # If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
 # Create a symbolic link named 'current' pointing to the latest release
 ln -sfn /data/web_static/releases/test/ /data/web_static/current
