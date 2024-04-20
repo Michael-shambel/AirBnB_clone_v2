74963ff54b2658d140fa83e334492d0c975d216e
michael-shambel <Michaelshambel10@gmail.com>
2024-04-20 08:32:58 +0000

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def hello_hbnb():
 	return "Hello HBNB!"
 
-@app.route('/hnbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
 	return "HBNB"
-if __name__ == "__main__":
+if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
