88deeafe4cb4019ae47bedaafc027aada81cfdc4
michael-shambel <Michaelshambel10@gmail.com>
2024-04-20 14:38:35 -0700

    task all

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,35 +4,50 @@ script that starts a Flask web application
 """
 from flask import Flask, render_template
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-        return "Hello HBNB!"
+    return "Hello HBNB!"
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-        return "HBNB"
+    return "HBNB"
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-        return "C " + text.replace("_", " ")
+    return "C " + text.replace("_", " ")
+
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_replace(text):
-        return "Python " + text.replace("_", " ")
+    return "Python " + text.replace("_", " ")
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
-        return "{} is a number".format(n)
+    return "{} is a number".format(n)
+
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-        if isinstance(n, int):
-                return render_template('5-number.html', n=n)
-        else:
-                return "Not Found", 404
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+    else:
+        return "Not Found", 404
+
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-	if isinstance(n, int):
-		return render_template('6-number_odd_or_even.html',
-				       n=n, even_odd=(
-				       'even' if n % 2 == 0 else 'odd'))
-	else:
-		return "Not Found", 404
+    if isinstance(n, int):
+        return render_template('6-number_odd_or_even.html',
+                               n=n, even_odd=('even' if n % 2 == 0 else 'odd'))
+    else:
+        return "Not Found", 404
+
+
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
