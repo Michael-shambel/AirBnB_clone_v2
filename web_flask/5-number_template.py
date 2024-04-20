b12e7f4db2358fffa01cf293950a8824a9d9ee18
michael-shambel <Michaelshambel10@gmail.com>
2024-04-20 11:40:57 +0000

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -23,7 +23,7 @@ def number_route(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
 	if isinstance(n, int):
-		return render_template('templates/5-number.html', n=n)
+		return render_template('5-number.html', n=n)
 	else:
 		return "Not Found", 404
 if __name__ == '__main__':
