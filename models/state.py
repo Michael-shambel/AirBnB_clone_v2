99f45b1f41ef72ba46fd95181b57548f07a3d6a4
michael-shambel <Michaelshambel10@gmail.com>
2024-04-20 15:47:16 -0700

    task 123

--- models/state.py
+++ models/state.py
@@ -13,9 +13,10 @@ class State(BaseModel, Base):
     name = ""
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
+        cities = relationship("City",  backref="state", cascade="delete")
+    else:
         @property
         def cities(self):
             """Get a list of all related City objects."""
