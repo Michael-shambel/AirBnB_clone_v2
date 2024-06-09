e6e5921d910f84f38024166a0da57365c0d01208
Michael-shambel <Michaelshambel10@gmail.com>
2024-06-09 14:08:11 -0700

    task 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-Fabric script that deletes out-of-date archives,
-using the function do_clean
+Fabric script that deletes out-of-date archives
 """
 from fabric.api import *
 from os.path import exists
 
-env.hosts = ['<IP web-01>', '<IP web-02>']
-env.user = 'ubuntu'
+env.hosts = ['54.174.253.226', '35.168.1.245']
 
 
 def do_clean(number=0):
@@ -17,22 +15,12 @@ def do_clean(number=0):
     try:
         number = int(number)
     except ValueError:
-        print("Invalid number provided.")
         return
 
-    if number < 0:
-        print("Number must be a non-negative integer.")
-        return
+    number = 1 if number == 0 else number
 
-    # Delete unnecessary archives in versions folder
-    local("ls -t versions | tail -n +{} | xargs -I {{}} rm versions/{{}}"
-          .format(number + 1))
+    with lcd('versions'):
+        local('ls -t | tail -n +{} | xargs -I {{}} rm {{}}'.format(number + 1))
 
-    for host in env.hosts:
-        with settings(host_string=host):
-            releases = run("ls -t /data/web_static/releases/")
-            releases_list = releases.split()
-            if len(releases_list) > number:
-                to_delete = releases_list[number:]
-                for archive in to_delete:
-                    run("rm -rf /data/web_static/releases/{}".format(archive))
+    with cd('/data/web_static/releases'):
+        run('ls -t | tail -n +{} | xargs -I {{}} rm -rf {{}}'.format(number + 1))
