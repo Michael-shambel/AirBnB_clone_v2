368c8aeef71e7626ef1b9ea9c5a245715fc5c73a
Michael-shambel <Michaelshambel10@gmail.com>
2024-06-10 13:47:17 -0700

    env2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
 """
 Fabric script that creates and distributes an archive to your web servers,
 using the function deploy
@@ -6,50 +6,8 @@ using the function deploy
 from fabric.api import *
 from datetime import datetime
 from os.path import exists
-
-env.hosts = ['54.174.253.226', '35.168.1.245']
-
-
-def do_pack():
-    """
-    All files in the folder web_static must be added to the final archive
-    """
-    local("mkdir -p versions")
-    time = datetime.now()
-    archive_path = "web_static_{}.tgz".format(time.strftime("%Y%m%d%H%M%S"))
-    result = local("tar -czvf versions/{} web_static".format(archive_path))
-    if result.failed:
-        return None
-    else:
-        return archive_path
-
-
-def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers
-    """
-    if not exists(archive_path):
-        return False
-
-    name = archive_path.split('/')[-1]
-    no_ext = name.split('.')[0]
-
-    try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(no_ext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            name, no_ext))
-        run('rm /tmp/{}'.format(name))
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'.format(no_ext, no_ext))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(no_ext))
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        return False
+do_pack = __import__('2-do_deploy_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 def deploy():
